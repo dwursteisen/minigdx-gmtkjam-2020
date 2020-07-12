3df524736828d64c24f42f3c1ab9dec4fe752beb
David Wursteisen <david.wursteisen+git@gmail.com>
2020-07-12 18:44:18 +0200

    GMTKJAM - fix itchi.io support

--- src/jsMain/kotlin/com/github/dwursteisen/minigdx/demo/Main.kt
+++ src/jsMain/kotlin/com/github/dwursteisen/minigdx/demo/Main.kt
@@ -10,6 +10,7 @@ import org.w3c.dom.Attr
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.get
 import kotlin.browser.document
+import kotlin.browser.window
 
 
 @ExperimentalStdlibApi
@@ -18,5 +19,12 @@ class GmtkJam(gameContext: GameContext) : GameSystem(gameContext, GmtkJamScreen(
 @ExperimentalStdlibApi
 fun main() {
     val canvas = document.getElementsByTagName("canvas")[0]!!
-    configuration(GLConfiguration(canvas = canvas as HTMLCanvasElement)).execute { GmtkJam(it) }
+    var rootPath = window.location.protocol + "//" + window.location.host + window.location.pathname
+    // make it itchio compatible
+    rootPath = rootPath.replace("index.html", "")
+    configuration(GLConfiguration(
+        canvas = canvas as HTMLCanvasElement,
+        rootPath = rootPath
+    )
+    ).execute { GmtkJam(it) }
 }
